6ef9e18398b89bccc3c4c518b115fa1fb525f248
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-17 18:34:03 -0700

    fixed a bunch of errors

--- 4_take_image.sh
+++ 4_take_image.sh
@@ -4,7 +4,8 @@ ssh pivotal@bacon.flood.pivotallabs.com '[[ -d /Volumes/bacon ]] && [[ -f lion.d
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil create -srcfolder /Volumes/bacon lion.dmg'
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo asr imagescan --source lion.dmg'
 set +e
-ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union /bin/ls -cr  /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_{1[1-9]-*,HEAD}.i386.hfs.dmg | tail -n +2 | xargs echo rm'
+ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union rm /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_HEAD.i386.hfs.dmg'
+ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union "/bin/ls -cr  /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_1[1-9]-*.i386.hfs.dmg | tail -n +2 | xargs echo rm"'
 set -e
 ssh pivotal@bacon.flood.pivotallabs.com 'export DATE=$(date +%y-%m-%d_%H-%M);
   scp -i /Users/pivotal/.ssh/id_union_deploy lion.dmg \
