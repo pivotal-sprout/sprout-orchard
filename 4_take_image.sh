6bd1d4ea92753761a346f00dccefe4417e2c1851
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-17 17:21:06 -0700

    renaming image name to lion HEAD, keeping the last two builds

--- 4_take_image.sh
+++ 4_take_image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 ssh pivotal@bacon.flood.pivotallabs.com '[[ -d /Volumes/bacon ]]'
-set +e
-ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union rm /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_{1[1-9]-*,latest}.i386.hfs.dmg'
-set -e
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil create -srcfolder /Volumes/bacon lion.dmg'
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo asr imagescan --source lion.dmg'
+set +e
+ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union /bin/ls -cr  /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_{1[1-9]-*,HEAD}.i386.hfs.dmg | tail -n +3 | xargs echo rm'
+set -e
 ssh pivotal@bacon.flood.pivotallabs.com 'export DATE=$(date +%y-%m-%d_%H-%M);
   scp -i /Users/pivotal/.ssh/id_union_deploy lion.dmg \
     deploy@union:/Volumes/PivotLand/DeployStudio/Masters/HFS/lion_${DATE}.i386.hfs.dmg;
   ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union \
-    ln /Volumes/PivotLand/DeployStudio/Masters/HFS/{lion_${DATE},lion_latest}.i386.hfs.dmg;'
+    ln /Volumes/PivotLand/DeployStudio/Masters/HFS/{lion_${DATE},lion_HEAD}.i386.hfs.dmg;'
