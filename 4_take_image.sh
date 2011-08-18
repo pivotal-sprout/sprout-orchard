2b5fb00404b04cdac00749795241e21452a03cc0
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-17 17:41:12 -0700

    removed lion.dmg so hdiutil wouldn't bomb-out when trying to create it.

--- 4_take_image.sh
+++ 4_take_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-ssh pivotal@bacon.flood.pivotallabs.com '[[ -d /Volumes/bacon ]]'
+ssh pivotal@bacon.flood.pivotallabs.com '[[ -d /Volumes/bacon ]] && [[ -f lion.dmg ]] && rm lion.dmg'
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil create -srcfolder /Volumes/bacon lion.dmg'
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo asr imagescan --source lion.dmg'
 set +e
-ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union /bin/ls -cr  /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_{1[1-9]-*,HEAD}.i386.hfs.dmg | tail -n +3 | xargs echo rm'
+ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union /bin/ls -cr  /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_{1[1-9]-*,HEAD}.i386.hfs.dmg | tail -n +2 | xargs echo rm'
 set -e
 ssh pivotal@bacon.flood.pivotallabs.com 'export DATE=$(date +%y-%m-%d_%H-%M);
   scp -i /Users/pivotal/.ssh/id_union_deploy lion.dmg \
