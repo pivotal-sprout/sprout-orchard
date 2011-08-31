dadec264b4fd197f924f87866f55974021ea44c9
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-31 15:58:21 -0700

    Pulls the mostly-pristine image from the DeployStudio server

--- 1_reset_machine.rb
+++ 1_reset_machine.rb
@@ -16,11 +16,17 @@ puts "detaching"
 # don't check return code; it says it failed even when it succeeds
 system("ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil detach /dev/disk0s2'")
 puts "restoring clean image"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo asr restore --buffers 1 --buffersize 32m --source Desktop/lion_ssh_sudo_autologin.dmg  --erase --noprompt --target /dev/disk0s2'")
+system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo asr restore --buffers 1 --buffersize 32m --source /Volumes/DeployStudio/Masters/HFS/lion_mostly_pristine.i386.hfs.dmg  --erase --noprompt --target /dev/disk0s2'")
 puts "attaching disk0s2 again"
 system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil attach /dev/disk0s2'")
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'cp .ssh/authorized_keys2 /Volumes/bacon/Users/pivotal/.ssh/'")
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'cp assets/auto_run.command /Volumes/bacon/Users/pivotal/bin/'")
+puts "renaming restored image to 'bacon'"
+system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo diskutil renameVolume /dev/disk0s2 bacon'")
+puts "putting ssh-keys into place"
+system!("ssh pivotal@bacon.flood.pivotallabs.com 'cp -Rp .ssh /Volumes/bacon/Users/pivotal/'")
+puts "now putting sudoers that doesn't ask for a password'"
+system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo cp {,/Volumes/bacon}/private/etc/sudoers'")
+puts "turn on sshd"
+system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo cp /etc/ssh* /Volumes/bacon/etc/; sudo defaults write /Volumes/bacon/var/db/launchd.db/com.apple.launchd/overrides.plist com.openssh.sshd -dict Disabled -bool false'")
 
 reboot_to("/Volumes/bacon")
 
