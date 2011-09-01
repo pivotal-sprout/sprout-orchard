cf68742bfbb9b56336cd61d61df119cab935b506
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-08-31 17:14:34 -0700

    move auto-rename autostart into place; git clone the apple_orchard repo

--- 2_run_soloist.sh
+++ 2_run_soloist.sh
@@ -2,7 +2,7 @@
 set -e
 
 ssh pivotal@bacon.flood.pivotallabs.com 'mkdir -p ~/workspace'
-ssh pivotal@bacon.flood.pivotallabs.com 'eval `ssh-agent` && ssh-add  ~/.ssh/id_github_lion && cd workspace && git clone git@github.com:pivotal/pivotal_workstation.git && git clone git@github.com:pivotalprivate/pivotal_workstation_private.git'
+ssh pivotal@bacon.flood.pivotallabs.com 'eval `ssh-agent` && ssh-add  ~/.ssh/id_github_lion && cd workspace && git clone git@github.com:pivotal/pivotal_workstation.git && git clone git@github.com:pivotalprivate/pivotal_workstation_private.git && git clone git@github.com:pivotalexperimental/apple_orchard.git'
 
 ssh pivotal@bacon.flood.pivotallabs.com 'cat > soloistrc <<EOF
 cookbook_paths:
@@ -16,8 +16,10 @@ EOF
 ssh pivotal@bacon.flood.pivotallabs.com 'gem list | grep soloist || sudo gem install soloist'
 ssh pivotal@bacon.flood.pivotallabs.com 'soloist'
 
-# clean-up
-ssh pivotal@bacon.flood.pivotallabs.com 'rm -fr ~/bin'
+# post-install
+ssh pivotal@bacon.flood.pivotallabs.com 'sudo cp ~/workspace/apple_orchard/assets/com.pivotallabs.auto_set_hostname.plist  /Library/LaunchAgents/'
+ssh pivotal@bacon.flood.pivotallabs.com 'cp ~/workspace/apple_orchard/assets/auto_set_hostname.rb ~/bin'
+#ssh pivotal@bacon.flood.pivotallabs.com 'rm -fr ~/bin'
 
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo bless --mount /Volumes/Persistent --setboot'
 ssh pivotal@bacon.flood.pivotallabs.com 'rm -fr ~/.ssh; sudo shutdown -r now'
