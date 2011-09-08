3015dcd08f4c40aeec4f0b5dde8d01e95a888e12
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-09-08 13:48:58 -0700

    fixed scp

--- 4_take_image.rb
+++ 4_take_image.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # set up one-time autorun
-system("scp",__FILE__.sub(/\/[^\/]*$/,"")  + "/assets/auto_set_hostname.rb","/Volumes/bacon/Users/pivotal/bin/auto_run.command")
+
+system("scp",__FILE__.sub(/\/[^\/]*$/,"")  + "/assets/auto_set_hostname.rb","pivotal@bacon.flood.pivotallabs.com:/Volumes/bacon/Users/pivotal/bin/auto_run.command")
 #set -e
 `ssh pivotal@bacon.flood.pivotallabs.com '[[ -d /Volumes/bacon ]] && [[ -f lion.dmg ]] && rm lion.dmg'`
 `ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil create -srcfolder /Volumes/bacon lion.dmg'`
@@ -9,8 +10,8 @@ system("scp",__FILE__.sub(/\/[^\/]*$/,"")  + "/assets/auto_set_hostname.rb","/Vo
 `ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union rm /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_HEAD.i386.hfs.dmg'`
 `ssh pivotal@bacon.flood.pivotallabs.com 'ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union "/bin/ls -cr  /Volumes/PivotLand/DeployStudio/Masters/HFS/lion_1[1-9]-*.i386.hfs.dmg | tail -n +2 | xargs rm"'`
 #set -e
-`ssh pivotal@bacon.flood.pivotallabs.com 'export DATE=$(date +%y-%m-%d_%H-%M);
-  scp -i /Users/pivotal/.ssh/id_union_deploy lion.dmg \
-    deploy@union:/Volumes/PivotLand/DeployStudio/Masters/HFS/lion_${DATE}.i386.hfs.dmg;
+date=`date +%y-%m-%d_%H-%M`.chop
+`ssh pivotal@bacon.flood.pivotallabs.com ' scp -i /Users/pivotal/.ssh/id_union_deploy lion.dmg \
+    deploy@union:/Volumes/PivotLand/DeployStudio/Masters/HFS/lion_#{date}.i386.hfs.dmg;
   ssh -i /Users/pivotal/.ssh/id_union_deploy deploy@union \
-    cd /Volumes/PivotLand/DeployStudio/Masters/HFS/; ln -s lion_{${DATE},HEAD}.i386.hfs.dmg;'`
+    cd /Volumes/PivotLand/DeployStudio/Masters/HFS/; ln -s lion_{#{date},HEAD}.i386.hfs.dmg;'`
