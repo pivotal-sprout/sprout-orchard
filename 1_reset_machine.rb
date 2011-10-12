e4e270e6d33d9afe3de65611ecd20d54d0f826b4
Brian Cunnie & Matthew Kocher <pair+cunnie+mkocher@pivotallabs.com>
2011-10-12 10:35:47 -0700

    pre-populate downloads 2/3 of the time

--- 1_reset_machine.rb
+++ 1_reset_machine.rb
@@ -14,22 +14,27 @@ end
 
 puts "detaching"
 # don't check return code; it says it failed even when it succeeds
-system("ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil detach /dev/disk0s2'")
+system("ssh pivotal@bacon 'sudo hdiutil detach /dev/disk0s2'")
 puts "restoring clean image"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo asr restore --buffers 1 --buffersize 32m --source /Volumes/DeployStudio/Masters/HFS/lion_mostly_pristine.i386.hfs.dmg  --erase --noprompt --target /dev/disk0s2'")
+system!("ssh pivotal@bacon 'sudo asr restore --buffers 1 --buffersize 32m --source /Volumes/DeployStudio/Masters/HFS/lion_mostly_pristine.i386.hfs.dmg  --erase --noprompt --target /dev/disk0s2'")
 puts "attaching disk0s2 again"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo hdiutil attach /dev/disk0s2'")
+system!("ssh pivotal@bacon 'sudo hdiutil attach /dev/disk0s2'")
 puts "renaming restored image to 'bacon'"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo diskutil renameVolume /dev/disk0s2 bacon'")
+system!("ssh pivotal@bacon 'sudo diskutil renameVolume /dev/disk0s2 bacon'")
 puts "turning off spotlight on /Volumes/bacon"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo mdutil -i off /Volumes/bacon'")
+system!("ssh pivotal@bacon 'sudo mdutil -i off /Volumes/bacon'")
 puts "putting ssh-keys into place"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'cp -Rp .ssh /Volumes/bacon/Users/pivotal/'")
+system!("ssh pivotal@bacon 'cp -Rp .ssh /Volumes/bacon/Users/pivotal/'")
 puts "now putting sudoers that doesn't ask for a password'"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo cp {,/Volumes/bacon}/private/etc/sudoers'")
+system!("ssh pivotal@bacon 'sudo cp {,/Volumes/bacon}/private/etc/sudoers'")
 puts "turn on sshd"
-system!("ssh pivotal@bacon.flood.pivotallabs.com 'sudo cp /etc/ssh* /Volumes/bacon/etc/; sudo defaults write /Volumes/bacon/var/db/launchd.db/com.apple.launchd/overrides.plist com.openssh.sshd -dict Disabled -bool false'")
-
+system!("ssh pivotal@bacon 'sudo cp /etc/ssh* /Volumes/bacon/etc/; sudo defaults write /Volumes/bacon/var/db/launchd.db/com.apple.launchd/overrides.plist com.openssh.sshd -dict Disabled -bool false'")
+now = Time.new.to_i;
+if now % 3 != 0
+  system("ssh pivotal@bacon 'sudo mkdir -p /Volumes/bacon/var/chef/cache; sudo chown pivotal:admin /Volumes/bacon/var/chef/cache'")
+  system("rsync -avH /var/chef/cache/ pivotal@bacon:/var/chef/cache/")
+  system("rsync -avH ~/Library/Caches/Homebrew pivotal@bacon:/Volumes/bacon/Users/pivotal/Library/Caches/")
+end
 reboot_to("/Volumes/bacon")
 
 disappear_reappear
