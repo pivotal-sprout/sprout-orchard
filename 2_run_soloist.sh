62483cb0327c52756250513f082dff4d4e88504f
Brian Cunnie <cunnie@pivotallabs.com>
2011-12-12 17:38:27 -0800

    after downloading, change the remote origin so that users can do a 'git pull' more easily

--- 2_run_soloist.sh
+++ 2_run_soloist.sh
@@ -2,7 +2,18 @@
 set -e
 
 ssh pivotal@bacon.flood.pivotallabs.com 'mkdir -p ~/workspace'
-ssh pivotal@bacon.flood.pivotallabs.com 'eval `ssh-agent` && ssh-add  ~/.ssh/id_github_lion && cd workspace && git clone https://github.com/pivotal/pivotal_workstation.git && git clone git@github.com:pivotalprivate/pivotal_workstation_private.git && git clone git@github.com:pivotalexperimental/apple_orchard.git'
+ssh pivotal@bacon.flood.pivotallabs.com 'eval `ssh-agent` && 
+  ssh-add  ~/.ssh/id_github_lion && 
+  cd workspace && 
+  git clone https://github.com/pivotal/pivotal_workstation.git && 
+  pushd /pivotal_workstation &&
+  git remote set-url origin https://github.com/pivotal/pivotal_workstation.git &&
+  popd
+  git clone git@github.com:pivotalprivate/pivotal_workstation_private.git && 
+  pushd pivotal_workstation_private &&
+  git remote set-url origin https://pivotalcommon@github.com/pivotalprivate/pivotal_workstation_private.git &&
+  popd &&
+  git clone git@github.com:pivotalexperimental/apple_orchard.git'
 
 ssh pivotal@bacon.flood.pivotallabs.com 'cat > soloistrc <<EOF
 cookbook_paths:
@@ -24,8 +35,7 @@ ssh pivotal@bacon.flood.pivotallabs.com 'sudo hostname NEWLY_IMAGED
   sudo diskutil rename /           NEWLY_IMAGED'
 
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo cp ~/workspace/apple_orchard/assets/com.pivotallabs.auto_set_hostname.plist  /Library/LaunchAgents/'
-ssh pivotal@bacon.flood.pivotallabs.com 'mkdir ~/bin; cp ~/workspace/apple_orchard/assets/auto_set_hostname.rb ~/bin'
-#ssh pivotal@bacon.flood.pivotallabs.com 'rm -fr ~/bin'
+ssh pivotal@bacon.flood.pivotallabs.com 'mkdir ~/bin; cp ~/workspace/apple_orchard/assets/auto_set_hostname.rb /usr/sbin/'
 
 ssh pivotal@bacon.flood.pivotallabs.com 'sudo bless --mount /Volumes/Persistent --setboot'
 ssh pivotal@bacon.flood.pivotallabs.com 'rm -fr ~/.ssh; sudo shutdown -r now'
