4a3219d58a2d186b20f9ecaf4bbc6320516e6c2a
Brian Cunnie <cunnie@pivotallabs.com>
2012-03-22 11:08:14 -0700

    asr has started to complain about fragmented catalogs, but it doesn't seem important.  Fix:  '--allowfragmentedcatalog'

--- 4_take_image.rb
+++ 4_take_image.rb
@@ -2,20 +2,20 @@
 # set up one-time autorun
 
 puts "removing now-useless .curlrc"
-system("rm /Volumes/NEWLY_IMAGED/{var/root,Users/#{ENV['IMAGER_USER']}/.curlrc")
+system("rm /Volumes/NEWLY_IMAGED/{var/root,Users/#{ENV['IMAGER_USER']}}/.curlrc")
 puts "clear out old lion.dmg"
 system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} '[[ -d /Volumes/NEWLY_IMAGED ]] && [[ -f lion.dmg ]] && rm lion.dmg'")
 puts "create new lion.dmg"
 system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'sudo hdiutil create -srcfolder /Volumes/NEWLY_IMAGED lion.dmg'")
 puts "imagescan new lion.dmg for restore"
-system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'sudo asr imagescan --source lion.dmg'")
+system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'sudo asr imagescan --allowfragmentedcatalog --source lion.dmg'")
 puts "remove old lion_HEAD from DeployStudio Masters"
 
 unless ENV['DEPLOYSTUDIO_SSH_KEYFILE'].nil? || ENV['DEPLOYSTUDIO_DESTDIR'].nil? || ENV['DEPLOYSTUDIO_USER_HOST'].nil?
   system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'ssh -i #{ENV['DEPLOYSTUDIO_SSH_KEYFILE']} #{ENV['DEPLOYSTUDIO_USER_HOST']} rm #{ENV['DEPLOYSTUDIO_DESTDIR']}/lion_HEAD.i386.hfs.dmg'")
   puts "remove all but the two most recent snapshots"
-  system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'ssh -i /Users/#{ENV['IMAGER_USER']}/.ssh/id_union_deploy #{ENV['DEPLOYSTUDIO_USER_HOST']} \"/bin/ls -cr  #{ENV['DEPLOYSTUDIO_DESTDIR']}/lion_1[1-9]-*.i386.hfs.dmg | tail -n +2 | xargs rm\"'")
-  date=`date +%y-%m-%d_%H-%M`.chop
+  system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'ssh -i /Users/#{ENV['IMAGER_USER']}/.ssh/id_union_deploy #{ENV['DEPLOYSTUDIO_USER_HOST']} \"/bin/ls -cr  #{ENV['DEPLOYSTUDIO_DESTDIR']}/lion_[0-9]*1[1-9]-*.i386.hfs.dmg | tail -n +2 | xargs rm\"'")
+  date=`date +%Y-%m-%d_%H-%M`.chop
   puts "copy the new timestamped image & link to lion_HEAD"
   system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} ' scp -i /Users/#{ENV['IMAGER_USER']}/.ssh/id_union_deploy lion.dmg \
     #{ENV['DEPLOYSTUDIO_USER_HOST']}:#{ENV['DEPLOYSTUDIO_DESTDIR']}/lion_#{date}.i386.hfs.dmg;
