8cc442bea029716df8d9d37a5bbf4820228d6a81
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-07-06 16:34:11 -0700

    removed deprecated PRISTINE_SOURCE

--- lib/util.rb
+++ lib/util.rb
@@ -1,11 +1,10 @@
-['IMAGE_DIR','IMAGER_USER','IMAGER_HOST','PRISTINE_SOURCE'].each do |env_variable|
+['IMAGE_DIR','IMAGER_USER','IMAGER_HOST'].each do |env_variable|
   raise "ENV['#{env_variable}'] is not set!" if ENV[env_variable].nil?
 end
 
 # export IMAGE_DIR=/Volumes/DeployStudio/Masters/HFS
 # export IMAGER_USER=pivotal
 # export IMAGER_HOST=bacon
-# export PRISTINE_SOURCE=/Volumes/DeployStudio/Masters/HFS
 # export DEPLOYSTUDIO_SSH_KEYFILE=/Users/pivotal/.ssh/id_union_deploy
 # export DEPLOYSTUDIO_DESTDIR=/Volumes/PivotLand/DeployStudio/Masters/HFS
 # export DEPLOYSTUDIO_USER_HOST=deploy@union
