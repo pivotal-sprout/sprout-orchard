578c4d55822939f37a7be7d1f20b7d1ca5794788
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-07-06 16:31:14 -0700

    removed deprecated DISK_PARTITION environment variable

--- lib/util.rb
+++ lib/util.rb
@@ -1,11 +1,10 @@
-['IMAGE_DIR','IMAGER_USER','IMAGER_HOST','DISK_PARTITION','PRISTINE_SOURCE'].each do |env_variable|
+['IMAGE_DIR','IMAGER_USER','IMAGER_HOST','PRISTINE_SOURCE'].each do |env_variable|
   raise "ENV['#{env_variable}'] is not set!" if ENV[env_variable].nil?
 end
 
 # export IMAGE_DIR=/Volumes/DeployStudio/Masters/HFS
 # export IMAGER_USER=pivotal
 # export IMAGER_HOST=bacon
-# export DISK_PARTITION=/dev/disk0s2
 # export PRISTINE_SOURCE=/Volumes/DeployStudio/Masters/HFS
 # export DEPLOYSTUDIO_SSH_KEYFILE=/Users/pivotal/.ssh/id_union_deploy
 # export DEPLOYSTUDIO_DESTDIR=/Volumes/PivotLand/DeployStudio/Masters/HFS
