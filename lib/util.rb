56c49fcb324526a2c7ce66037c5440d223a9937d
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-07-13 11:51:25 -0700

    One last IMAGER -> IMAGE change

--- lib/util.rb
+++ lib/util.rb
@@ -1,35 +1,31 @@
-['IMAGE_DIR','IMAGER_USER','IMAGER_HOST'].each do |env_variable|
+['IMAGE_DIR','IMAGE_USER','IMAGE_HOST'].each do |env_variable|
   raise "ENV['#{env_variable}'] is not set!" if ENV[env_variable].nil?
 end
 
-# export IMAGE_DIR=/Volumes/DeployStudio/Masters/HFS
-# export IMAGER_USER=pivotal
-# export IMAGER_HOST=bacon
-# export DEPLOYSTUDIO_SSH_KEYFILE=/Users/pivotal/.ssh/id_union_deploy
-# export DEPLOYSTUDIO_DESTDIR=/Volumes/PivotLand/DeployStudio/Masters/HFS
-# export DEPLOYSTUDIO_USER_HOST=deploy@union
+image_user = ENV['IMAGE_USER'];
+image_user_at_host = image_user + '@' + ENV['IMAGE_HOST']
 
 require 'timeout'
 
 module Util
   def on_persistent?
     # sometimes the disks aren't mounted; mount both disks to make sure
-    system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} -o ConnectTimeout=5 'sudo hdid #{$persistent_partition}; sudo hdid #{$newly_imaged_partition}; df' | grep '/Volumes/NEWLY_IMAGED'")
+    system("ssh #{image_user_at_host} -o ConnectTimeout=5 'sudo hdid #{$persistent_partition}; sudo hdid #{$newly_imaged_partition}; df' | grep '/Volumes/NEWLY_IMAGED'")
   end
 
   def reboot_to(volume)
     puts("rebooting to #{volume}")
-    system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'sudo hdiutil attach /dev/disk0'")
+    system("ssh #{image_user_at_host} 'sudo hdiutil attach /dev/disk0'")
     puts("Blessing #{volume}")
-    system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'sudo bless --mount #{volume} --setboot'")
+    system("ssh #{image_user_at_host} 'sudo bless --mount #{volume} --setboot'")
     puts("shutting down")
-    system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} 'sudo shutdown -r now'")
+    system("ssh #{image_user_at_host} 'sudo shutdown -r now'")
   end
 
   def disappear_reappear
     # wait for machine to disappear
     Timeout::timeout(120) do
-      if system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} -o ConnectTimeout=5 'true'")
+      if system("ssh #{image_user_at_host} -o ConnectTimeout=5 'true'")
         sleep 1
       end
     end
@@ -38,7 +34,7 @@ module Util
 
     # wait for machine to reappear
     Timeout::timeout(120) do
-      until system("ssh #{ENV['IMAGER_USER']}@#{ENV['IMAGER_HOST']} -o ConnectTimeout=5 'true'")
+      until system("ssh #{image_user_at_host} -o ConnectTimeout=5 'true'")
         sleep 1
       end
     end
