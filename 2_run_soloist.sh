092b2265882de7e4606f5a4e9400cdf860fcadf6
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-09-04 18:05:28 -0700

    Got rid of annoying "VMware Shared Folders" on Desktop

--- 2_run_soloist.sh
+++ 2_run_soloist.sh
@@ -49,6 +49,15 @@ ssh $IMAGE_USER@$IMAGE_HOST 'sudo hostname NEWLY_IMAGED
 ssh $IMAGE_USER@$IMAGE_HOST 'sudo cp ~/workspace/apple_orchard/assets/com.pivotallabs.auto_set_hostname.plist  /Library/LaunchAgents/'
 ssh $IMAGE_USER@$IMAGE_HOST 'mkdir ~/bin; sudo cp ~/workspace/apple_orchard/assets/auto_set_hostname.rb /usr/sbin/'
 
-ssh $IMAGE_USER@$IMAGE_HOST 'launchctl unload -w /Library/LaunchAgents/com.vmware.launchd.vmware-tools-userd.plist; true'
+# turn off vmware tools (VMware Shared Folders) if installed
+ssh $IMAGE_USER@$IMAGE_HOST 'PLIST=/Library/LaunchAgents/com.vmware.launchd.vmware-tools-userd.plist; 
+  [ -f $PLIST ] && 
+  sudo defaults write $PLIST RunAtLoad -bool false &&
+  sudo plutil -convert xml1 $PLIST &&
+  sudo chmod 444 $PLIST
+  rm ~/Desktop/VMWare\ Shared\ Folders
+  true'
+
+# reboot to Persistent
 ssh $IMAGE_USER@$IMAGE_HOST 'sudo bless --mount /Volumes/Persistent --setboot'
 ssh $IMAGE_USER@$IMAGE_HOST 'rm -fr ~/.ssh; sudo shutdown -r now'
