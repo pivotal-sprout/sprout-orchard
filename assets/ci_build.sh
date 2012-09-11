c9657f416a827b90f0a23765df3626ceaf40aaf7
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-09-11 13:55:59 -0700

    chef gem no longer locked to specific rev

--- assets/ci_build.sh
+++ assets/ci_build.sh
@@ -7,7 +7,7 @@ rm -rf ~/cookbooks/pivotal_workstation*
 git clone git@github.com:pivotalprivate/pivotal_workstation_private.git
 git clone https://github.com/pivotal/pivotal_workstation.git
 
-sudo gem install chef --version 0.10.8
+sudo gem install chef
 sudo gem install soloist
 
 LOG_LEVEL=debug soloist
