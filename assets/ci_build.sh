c6cae4f028a676e7540e549570c0136578c7d8bc
Brian Cunnie <pair+cunnie@pivotallabs.com>
2012-09-17 13:20:51 -0700

    pre-populate github key, this time for sure

--- assets/ci_build.sh
+++ assets/ci_build.sh
@@ -1,10 +1,16 @@
 # Run this script on the ci box, not the jenkins box
+
+# grab the github key, which will exit non-zero
+ssh -o StrictHostKeyChecking=no git@github.com
+
+# start watching for errors
 set -e
 
 mkdir -p ~/cookbooks
 cd ~/cookbooks
 
 rm -rf ~/cookbooks/pivotal_workstation*
+# FIXME: the following line is pivotal-internal
 git clone git@github.com:pivotalprivate/pivotal_workstation_private.git
 git clone https://github.com/pivotal/pivotal_workstation.git
 
