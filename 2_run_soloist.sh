f7c3b12a1c9067cc1cfd863885acb4fc9ace5a9d
Dennis Schmidt & Matthew Kocher <pair+dennis+matthew@pivotallabs.com>
2012-11-12 18:18:34 -0800

    use prerelease soloist

--- 2_run_soloist.sh
+++ 2_run_soloist.sh
@@ -3,8 +3,8 @@ set -e
 
 ssh $IMAGE_USER@$IMAGE_HOST 'mkdir -p ~/cookbooks; mkdir -p ~/workspace'
 ssh $IMAGE_USER@$IMAGE_HOST "cd cookbooks &&
-  git clone https://github.com/pivotal/pivotal_workstation.git && 
-  git clone https://github.com/opscode-cookbooks/dmg.git && 
+  git clone https://github.com/pivotal/pivotal_workstation.git &&
+  git clone https://github.com/opscode-cookbooks/dmg.git &&
   cd pivotal_workstation &&
   git checkout $GIT_BRANCH &&
   cd ~/workspace &&
@@ -22,11 +22,11 @@ EOF
 '
 
 if [[ $PIVOTAL_LABS ]]; then
-  ssh $IMAGE_USER@$IMAGE_HOST 'eval `ssh-agent` && 
-    ssh-add  ~/.ssh/id_github_lion && 
+  ssh $IMAGE_USER@$IMAGE_HOST 'eval `ssh-agent` &&
+    ssh-add  ~/.ssh/id_github_lion &&
     pushd cookbooks &&
-    ( ssh -o StrictHostKeyChecking=no git@github.com exit; : ) && 
-    git clone git@github.com:pivotalprivate/pivotal_workstation_private.git && 
+    ( ssh -o StrictHostKeyChecking=no git@github.com exit; : ) &&
+    git clone git@github.com:pivotalprivate/pivotal_workstation_private.git &&
     pushd pivotal_workstation_private &&
     git remote set-url origin https://pivotalcommon@github.com/pivotalprivate/pivotal_workstation_private.git &&
     popd && popd &&
@@ -34,8 +34,7 @@ if [[ $PIVOTAL_LABS ]]; then
     echo "- pivotal_workstation_private::meta_lion_image" >> ~/soloistrc'
 fi
 
-ssh $IMAGE_USER@$IMAGE_HOST 'gem list | grep chef || sudo gem install chef'
-ssh $IMAGE_USER@$IMAGE_HOST 'gem list | grep soloist || sudo gem install soloist'
+ssh $IMAGE_USER@$IMAGE_HOST 'gem list | grep soloist || sudo gem install soloist --pre'
 ssh $IMAGE_USER@$IMAGE_HOST 'soloist'
 
 # post-install, set the machine name to NEWLY_IMAGED
@@ -49,8 +48,8 @@ ssh $IMAGE_USER@$IMAGE_HOST 'sudo cp ~/workspace/apple_orchard/assets/com.pivota
 ssh $IMAGE_USER@$IMAGE_HOST 'mkdir ~/bin; sudo cp ~/workspace/apple_orchard/assets/auto_set_hostname.rb /usr/sbin/'
 
 # turn off vmware tools (VMware Shared Folders) if installed
-ssh $IMAGE_USER@$IMAGE_HOST 'PLIST=/Library/LaunchAgents/com.vmware.launchd.vmware-tools-userd.plist; 
-  [ -f $PLIST ] && 
+ssh $IMAGE_USER@$IMAGE_HOST 'PLIST=/Library/LaunchAgents/com.vmware.launchd.vmware-tools-userd.plist;
+  [ -f $PLIST ] &&
   sudo defaults write $PLIST RunAtLoad -bool false &&
   sudo plutil -convert xml1 $PLIST &&
   sudo chmod 444 $PLIST
