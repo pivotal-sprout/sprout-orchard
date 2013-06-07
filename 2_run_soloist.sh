8f8e8a8bd6f4c67ed684277ae7d264df387fd62b
Brian Cunnie <pair+cunnie@pivotallabs.com>
2013-06-07 15:16:32 -0700

    pivotal_workstation_private repo under https://github.com/pivotal

--- 2_run_soloist.sh
+++ 2_run_soloist.sh
@@ -11,7 +11,7 @@ if [[ $PIVOTAL_LABS ]]; then
     ssh-add  ~/.ssh/id_github_private &&
     ( ssh -o StrictHostKeyChecking=no git@github.com exit; : ) &&
     cd /tmp &&
-    git clone git@github.com:pivotalprivate/pivotal_workstation_private.git &&
+    git clone git@github.com:pivotal/pivotal_workstation_private.git &&
     echo "cookbook \"pivotal_workstation_private\", :path => \"/tmp/pivotal_workstation_private\"" >> /tmp/sprout-wrap/Cheffile'
 fi
 
